10dd88bea730c18b787754abcf9eecc0ff019c13
gvaugha <66960466+gvaugha@users.noreply.github.com>
2021-09-19 13:51:07 +0100

    Add files via upload

---
+++ GDV-OSDCloud.ps1
@@ -0,0 +1,25 @@
+Write-Host  -ForegroundColor Yellow "Starting Brooks' Custom OSDCloud ..."
+cls
+Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
+Write-Host "====================@gavaugha =====================" -ForegroundColor Yellow
+Write-Host "=======================================================" -ForegroundColor Yellow
+Write-Host "1: Zero-Touch Win10 21H1 | English | Enterprise"-ForegroundColor Yellow
+Write-Host "2: Zero-Touch Win10 20H2 | English | Enterprise" -ForegroundColor Yellow
+Write-Host "3: I'll select it myself"-ForegroundColor Yellow
+Write-Host "4: Exit`n"-ForegroundColor Yellow
+$input = Read-Host "Please make a selection"
+
+Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
+
+Import-Module OSD -Force
+Install-Module OSD -Force
+
+switch ($input)
+{
+    '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI } 
+    '2' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI } 
+    '3' { Start-OSDCloud	} 
+    '4' { Exit		}
+}
+
+wpeutil reboot
